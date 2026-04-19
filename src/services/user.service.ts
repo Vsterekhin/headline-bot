90810da172027de3e1ab0650aeecd67a2aa1af02
Vsterekhin <vsterekhin@gmail.com>
2026-04-19 18:30:49 +0300

    Update user.service.ts

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,19 +1,11 @@
-import { prisma } from "../db/prisma.js";
 import type { UserContext } from "../types/index.js";
 
 export async function upsertUser(context: UserContext) {
-  return prisma.user.upsert({
-    where: { telegramId: BigInt(context.telegramId) },
-    update: {
-      username: context.username,
-      firstName: context.firstName,
-      lastName: context.lastName
-    },
-    create: {
-      telegramId: BigInt(context.telegramId),
-      username: context.username,
-      firstName: context.firstName,
-      lastName: context.lastName
-    }
-  });
+  return {
+    id: context.telegramId,
+    telegramId: BigInt(context.telegramId),
+    username: context.username,
+    firstName: context.firstName,
+    lastName: context.lastName
+  };
 }
